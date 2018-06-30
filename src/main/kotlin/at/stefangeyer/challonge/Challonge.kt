9c9d4dc1b408ea766051e322072dafd6882af734
Stefan Geyer <stefangeyer@outlook.com>
2018-06-30 20:26:09 +0200

    Implemented Challonge which combines all services and should be used to access the api

---
+++ src/main/kotlin/at/stefangeyer/challonge/Challonge.kt
@@ -0,0 +1,30 @@
+package at.stefangeyer.challonge
+
+import at.stefangeyer.challonge.model.Credentials
+import at.stefangeyer.challonge.rest.client.RestClientFactory
+import at.stefangeyer.challonge.rest.client.retrofit.RetrofitRestClientFactory
+import at.stefangeyer.challonge.serializer.Serializer
+import at.stefangeyer.challonge.service.AttachmentService
+import at.stefangeyer.challonge.service.MatchService
+import at.stefangeyer.challonge.service.ParticipantService
+import at.stefangeyer.challonge.service.TournamentService
+import at.stefangeyer.challonge.service.implementation.SimpleAttachmentService
+import at.stefangeyer.challonge.service.implementation.SimpleMatchService
+import at.stefangeyer.challonge.service.implementation.SimpleParticipantService
+import at.stefangeyer.challonge.service.implementation.SimpleTournamentService
+
+class Challonge(credentials: Credentials, serializer: Serializer,
+                private val restClientFactory: RestClientFactory = RetrofitRestClientFactory(credentials, serializer)) {
+
+    val tournaments: TournamentService
+    val participants: ParticipantService
+    val matches: MatchService
+    val attachments: AttachmentService
+
+    init {
+        this.tournaments = SimpleTournamentService(this.restClientFactory.createTournamentRestClient())
+        this.participants = SimpleParticipantService(this.restClientFactory.createParticipantRestClient())
+        this.matches = SimpleMatchService(this.restClientFactory.createMatchRestClient())
+        this.attachments = SimpleAttachmentService(this.restClientFactory.createAttachmentRestClient())
+    }
+}
